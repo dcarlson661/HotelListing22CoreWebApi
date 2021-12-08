b6b8ad4e89647f649b984f4adbb6a4dfa63708f2
davidcarlson <dcarl@earthlink.net>
2021-12-08 14:30:43 -0800

    Add project files.

---
+++ CoreWebApi/Program.cs
@@ -0,0 +1,39 @@
+
+
+using Serilog;  //add in the proj file <PackageReference Include="Serilog.AspNetCore" Version="4.1.0" />
+using Serilog.Events;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Host.UseSerilog((ctx, lc) => lc
+    .WriteTo.Console()
+    .WriteTo.File(path: "C:\\SafeenvMeterPayloads\\log\\HotelListing22Logs\\log-.txt",
+                  outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                  rollingInterval: RollingInterval.Day,
+                  restrictedToMinimumLevel: LogEventLevel.Information
+    ));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+Log.Information("Application is starting");
+app.Run();
---
+++ CoreWebApi/readme.md
@@ -0,0 +1,16 @@
+﻿
+###### Heading Six
+#####  Heading Five
+####   Heading Four
+###    Heading Three
+##     Heading Two
+#      Heading One
+
+# https://docs.github.com/en/github/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
+
+# Serilog - first add this to the .proj file
+<PackageReference Include="Serilog.AspNetCore" Version="4.1.0" />
+
+# SSL developer cert - this has the preferred way to add the developer cert
+https://docs.microsoft.com/en-us/troubleshoot/visualstudio/general/warnings-untrusted-certificate
+
