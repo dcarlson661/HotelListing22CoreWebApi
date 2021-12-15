de0db2d248d4661ef0f4f29a0d3f9fdf9098e534
davidcarlson <dcarl@earthlink.net>
2021-12-15 11:30:15 -0800

    add code bodies to unitofwork.cs

--- CoreWebApi/IRepository/IUnitOfWork.cs
+++ CoreWebApi/IRepository/IUnitOfWork.cs
@@ -1,6 +1,14 @@
-﻿namespace HotelListing22CoreWebApi.IRepository
+﻿using HotelListing22CoreWebApi.Data;
+
+namespace HotelListing22CoreWebApi.IRepository
 {
-    public interface IUnitOfWork
+    public interface IUnitOfWork : IDisposable
     {
+        // these generic interfaces
+        //  will generate a lamba public IGenericRepository<Country> Countries => throw new NotImplementedException();
+        //  in classes that implement this interface
+        IGenericRepository<Country> Countries { get; }
+        IGenericRepository<Hotel> Hotels { get; }
+        Task Save();
     }
 }
---
+++ CoreWebApi/Repository/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using HotelListing22CoreWebApi.Data;
+using HotelListing22CoreWebApi.IRepository;
+
+namespace HotelListing22CoreWebApi.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+        private IGenericRepository<Country> _countries;
+        private IGenericRepository<Hotel> _hotels;
+
+        public UnitOfWork(DatabaseContext context)
+        {
+            this._context = context;
+        }
+
+        // ??= isnull conditional.   _countries is a property with only a getter { get; } in the interface
+        public IGenericRepository<Country> Countries => _countries ??= new GenericRepository<Country>(_context);
+
+        public IGenericRepository<Hotel> Hotels => _hotels ??= new GenericRepository<Hotel>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
